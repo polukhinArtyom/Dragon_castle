d3371f0ccc4039b00fb46d7bf8bf0f5aa581229d
Artyom Polukhin <50484298+polukhinArtyom@users.noreply.github.com>
2019-05-12 19:57:12 +0300

    Update Dragon castle v2.py

--- Dragon castle v2.py
+++ Dragon castle v2.py
@@ -1,6 +1,6 @@
 print("You are in a dark room in a mysterious castle.")
-print("In front of you are three door. You must choose one.")
-playerChoice = input("Choose 1, 2, or 3...")
+print("In front of you are four door. You must choose one.")
+playerChoice = input("Choose 1, 2, 3 or 4...\n")
 if playerChoice == "1":
     print("You find a room full of treasure. You're rich!")
     print("GAME OVER, YOU WIN!")
@@ -9,8 +9,18 @@ elif playerChoice == "2":
     print("GAME OVER, YOU LOSE!")
 elif playerChoice == "3":
     print("You go into the room and find a sleeping dragon.")
-    print("The dragon wakes up and eats you. You are delicious.")
-    print("GAME OVER, YOU LOSE!")
+    print("You can either:")
+    print("1) Try to steel some of the dragon's gold.")
+    print("2) Try to sneak around the dragon to the exit.")
+    dragonChoice = input("Type 1 or 2...")
+    if dragonChoice == "1":
+        print("The dragon wakes up and eats you. You are delicious.")
+        print("GAME OVER, YOU LOSE.")
+        elif dragonChoice == "2":
+            print("You sneak around the dragon and escape the castle, blinking in the sunshine.")
+            print("GAME OVER, YOU WIN!")
+        else:
+            print("Sorry, you didn't enter 1 or 2")
 else:
     print("Sorry, you didn't enter 1, 2 or 3!")
     print("Run the game again to have another go.")
